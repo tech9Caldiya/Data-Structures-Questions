ec399fced0e149a6617f041901997ad979cfd373
Diya Parmeshwari Sharma <117832108+tech9Caldiya@users.noreply.github.com>
2023-08-04 21:41:52 +0530

    015) Check if the Sentence is Pangram.java

--- 015) Check if the Sentence is Pangram.java
+++ 015) Check if the Sentence is Pangram.java
@@ -2,9 +2,14 @@ class Solution {
     public boolean checkIfPangram(String sentence) {
         boolean ans[] = new boolean[26]; // new boolean 
         boolean check = false;
+
+        // we are making a boolean array named ans and
+        //now we will be updating values of this new array to 'true' based on the character we get from input
+        // then we will traverse the boolean array if we have any false present this means that it is not a pangram
+
         for(int i = 0; i<sentence.length() ; i++)
         {
-            ans[ sentence.charAt(i)-97] = true;
+            ans[ sentence.charAt(i)-97] = true; //every character will be placed in bool array using charAt() method in form of true or false.
         }
         for(int j = 0 ; j< ans.length ; j++) // j not i! 
         {
