50fc8e7ea81c16877b247a5028dc37b839f54862
Diya Parmeshwari Sharma <117832108+tech9Caldiya@users.noreply.github.com>
2023-08-14 23:49:34 +0530

    023) Final value of variable after performing operations.java

---
+++ 023) Final value of variable after performing operations.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int finalValueAfterOperations(String[] opp) {
+        int X = 0 ;
+        for(int i = 0 ; i<opp.length ; i++)
+        {
+            if("X--".equals(opp[i]) ||
+             "--X".equals(opp[i]))
+            {
+                X = X - 1;
+            }
+            
+            if("X++".equals(opp[i]) ||
+            "++X".equals(opp[i]))
+            {
+                X = X + 1;
+            }
+        }
+        return X;
+        
+    }
+}
+url : https://leetcode.com/problems/final-value-of-variable-after-performing-operations/
