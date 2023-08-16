9f6499bfdaef26d6f5feb1b81dc060014e8241b6
Diya Parmeshwari Sharma <117832108+tech9Caldiya@users.noreply.github.com>
2023-08-16 23:38:00 +0530

    024) Maximum Number of Words Found in Sentences.java

---
+++ 024) Maximum Number of Words Found in Sentences.java
@@ -0,0 +1,22 @@
+import java.lang.Math;
+class Solution {
+    public int mostWordsFound(String[] sentences) {
+        int count = 0;
+        int max = 0;
+        // char ch = ' ';
+        for(int i = 0 ; i< sentences.length ; i++)
+        {
+            count = 1;
+            for(int j = 0 ; j<sentences[i].length() ; j++){
+            if(sentences[i].charAt(j) == ' ')
+            {
+                count++;
+            }   
+        }
+        max = Math.max(count , max);
+    }
+        // count ++;
+        return max;   
+    }
+}
+url: https://leetcode.com/problems/maximum-number-of-words-found-in-sentences/
