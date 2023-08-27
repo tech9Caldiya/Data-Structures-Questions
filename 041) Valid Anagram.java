d4fbcd054f86fa77c43acc73397cdec8f2ecc308
Diya Parmeshwari Sharma <117832108+tech9Caldiya@users.noreply.github.com>
2023-08-28 00:26:22 +0530

    041) Valid Anagram.java

---
+++ 041) Valid Anagram.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        
+        char[] s1 = s.toLowerCase().toCharArray();
+        char[] t1 = t.toLowerCase().toCharArray();
+        
+        if(s1.length != t1.length) return false;
+
+        Arrays.sort(s1);
+        Arrays.sort(t1);
+        for(int i = 0 ; i < s1.length ; i++){
+            if(s1[i] != t1[i])
+            return false;
+        }
+        return true;      
+    }
+}
