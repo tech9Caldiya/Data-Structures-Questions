58cf44d2ef864889d8671aa824894b26e02648b8
Diya Parmeshwari Sharma <117832108+tech9Caldiya@users.noreply.github.com>
2023-08-29 09:18:40 +0530

    042) find-n-unique-integers-sum-up-to-zero.java

--- 042) find-n-unique-integers-sum-up-to-zero.java
+++ 042) find-n-unique-integers-sum-up-to-zero.java
@@ -1,17 +1,25 @@
 class Solution {
     public int[] sumZero(int n) {
         int ans[] = new int[n];
-        int index = 0;
+        int mid = n/2 +1;
+        int i = 1;
+    
+        int start = 0;
+        int end = ans.length-1;
+        while(start<end){
 
-        if(n%2==0){
-            ans[index++]=0;
-        }
-        n = n/2;
-        for(int i = 1 ; i<=n ; i++)
+        if(n%2 != 0)
         {
-            ans[index++]=i;
-            ans[index++]= -i;
+            ans[mid] = 0;
+        }
+        ans[start] = i;
+        ans[end] = -i;
+        i++;
+        start++;
+        end--;
+            
         }
         return ans;
+        
     }
 }
